0e225df94cf00cdf30496d11693bc542d3d8293b
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-10-11 01:02:31 +0530

    Update RegularExpressionMatching.cpp

--- hard/RegularExpressionMatching.cpp
+++ hard/RegularExpressionMatching.cpp
@@ -1,3 +1,19 @@
+/* 10. Regular Expression Matching
+Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+ 
+
+Example 1:
+
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+*/
+
 class Solution {
 public:
     bool isMatch(string s, string p) {
