829b390b33f2edb0d441db31d6b7033c506ceb3e
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-10-14 11:56:07 +0530

    Create reverseLinkedList.java

---
+++ easy/reverseLinkedList.java
@@ -0,0 +1,12 @@
+//206 Reverse Linked List\
+class Solution {
+  public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null)
+      return head;
+
+    ListNode newHead = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return newHead;
+  }
+}
