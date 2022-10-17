5e9a9a076f3b2dd1cb2652481d03f694871fdf50
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-10-17 19:27:35 +0530

    Create First Unique Character in a String.py.py

---
+++ easy/First Unique Character in a String.py.py
@@ -0,0 +1,10 @@
+#387. First Unique Character in a String
+class Solution:
+  def firstUniqChar(self, s: str) -> int:
+    count = Counter(s)
+
+    for i, c in enumerate(s):
+      if count[c] == 1:
+        return i
+
+    return -1
