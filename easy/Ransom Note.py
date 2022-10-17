f1e7bd31ff88b83609b32168bd10e11342e3f341
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-10-17 19:28:29 +0530

    Create Ransom Note.py

---
+++ easy/Ransom Note.py
@@ -0,0 +1,6 @@
+#383. Ransom Note
+class Solution:
+  def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+    count1 = Counter(ransomNote)
+    count2 = Counter(magazine)
+    return all(count1[c] <= count2[c] for c in string.ascii_lowercase)
