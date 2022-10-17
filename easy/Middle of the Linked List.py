3e5b8fe1ff68294cdc467dcc9c89b991db0fc723
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-10-17 17:31:15 +0530

    Create Middle of the Linked List.py

---
+++ easy/Middle of the Linked List.py
@@ -0,0 +1,11 @@
+#876 Middle of The Linked List
+class Solution:
+  def middleNode(self, head: ListNode) -> ListNode:
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+      slow = slow.next
+      fast = fast.next.next
+
+    return slow
