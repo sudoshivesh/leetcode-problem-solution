ff6e945600980e115afdf382e16577698ab82c1b
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-10-16 23:24:32 -0700

    Create N-ary Tree Preorder Traversal.java

---
+++ easy/N-ary Tree Preorder Traversal.java
@@ -0,0 +1,20 @@
+// 589. N-ary Tree Preorder Traversal
+class Solution {
+  public List<Integer> preorder(Node root) {
+    if (root == null)
+      return new ArrayList<>();
+
+    List<Integer> ans = new ArrayList<>();
+    Deque<Node> stack = new ArrayDeque<>();
+    stack.push(root);
+
+    while (!stack.isEmpty()) {
+      root = stack.pop();
+      ans.add(root.val);
+      for (int i = root.children.size() - 1; i >= 0; --i)
+        stack.push(root.children.get(i));
+    }
+
+    return ans;
+  }
+}
