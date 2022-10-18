a785d21440eb010427b47dd33fb631c6c377ddfd
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-10-18 12:44:06 +0530

    Create Max Consecutive Ones.py

---
+++ easy/Max Consecutive Ones.py
@@ -0,0 +1,14 @@
+#485 Max Consecutive Ones
+class Solution:
+  def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+    ans = 0
+    summ = 0
+
+    for num in nums:
+      if num == 0:
+        summ = 0
+      else:
+        summ += num
+        ans = max(ans, summ)
+
+    return ans
