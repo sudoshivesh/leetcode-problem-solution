ae2e5c9449c99e14115b7efe987c749ae4757468
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-10-23 20:00:59 +0530

    Create Set Mismatch.py

---
+++ easy/Set Mismatch.py
@@ -0,0 +1,12 @@
+#645. Set Mismatch
+class Solution:
+  def findErrorNums(self, nums: List[int]) -> List[int]:
+    for num in nums:
+      if nums[abs(num) - 1] < 0:
+        duplicate = abs(num)
+      else:
+        nums[abs(num) - 1] *= -1
+
+    for i, num in enumerate(nums):
+      if num > 0:
+        return [duplicate, i + 1]
