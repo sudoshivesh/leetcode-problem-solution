07af19de2dc66183b63e58ab65a89841b1dee266
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-10-25 20:09:05 +0530

    Create Backspace String Compare.java

---
+++ easy/Backspace String Compare.java
@@ -0,0 +1,18 @@
+//844. Backspace String Compare
+class Solution {
+  public boolean backspaceCompare(String S, String T) {
+    return backspace(S).equals(backspace(T));
+  }
+
+  private String backspace(final String s) {
+    StringBuilder sb = new StringBuilder();
+
+    for (final char c : s.toCharArray())
+      if (c != '#')
+        sb.append(c);
+      else if (sb.length() != 0)
+        sb.deleteCharAt(sb.length() - 1);
+
+    return sb.toString();
+  }
+}
