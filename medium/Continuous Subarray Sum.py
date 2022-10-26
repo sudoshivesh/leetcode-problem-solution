375f3c50a1e182e352b45bb25cf7f3ba0f9c8bc0
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-10-26 21:02:36 +0530

    Create Continuous Subarray Sum.py

---
+++ medium/Continuous Subarray Sum.py
@@ -0,0 +1,17 @@
+#523. Continuous Subarray Sum
+class Solution:
+  def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+    prefix = 0
+    prefixToIndex = {0: -1}
+
+    for i, num in enumerate(nums):
+      prefix += num
+      if k != 0:
+        prefix %= k
+      if prefix in prefixToIndex:
+        if i - prefixToIndex[prefix] > 1:
+          return True
+      else:
+        prefixToIndex[prefix] = i
+
+    return False
