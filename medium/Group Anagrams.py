18ea61dc52c6bd62e7b0fa13ad31584a6dd48546
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-10-28 20:06:23 +0530

    Create Group Anagrams.py

---
+++ medium/Group Anagrams.py
@@ -0,0 +1,10 @@
+#49. Group Anagrams
+class Solution:
+  def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    dict = defaultdict(list)
+
+    for str in strs:
+      key = ''.join(sorted(str))
+      dict[key].append(str)
+
+    return dict.values()
