7460c8eb934a784e116852e00e2d2f791019a6ea
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-11-20 10:58:50 +0530

    Create Ugly Number.py

---
+++ easy/Ugly Number.py
@@ -0,0 +1,11 @@
+#263. Ugly Number
+class Solution:
+  def isUgly(self, n: int) -> bool:
+    if n == 0:
+      return False
+
+    for prime in 2, 3, 5:
+      while n % prime == 0:
+        n //= prime
+
+    return n == 1
