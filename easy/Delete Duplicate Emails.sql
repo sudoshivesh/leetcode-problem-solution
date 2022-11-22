6ce57a561646589a37a9839cc4182e0a9cc8c713
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-11-23 03:15:26 +0530

    Create Delete Duplicate Emails.sql

---
+++ easy/Delete Duplicate Emails.sql
@@ -0,0 +1,5 @@
+--196. Delete Duplicate Emails--
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+delete A from Person A, Person B 
+where A.id > B.id and A.email=B.email;
