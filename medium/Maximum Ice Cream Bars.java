f0e707f805108f61bdaa5281c7a77470a6703c30
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2023-01-06 23:17:03 +0530

    Create Maximum Ice Cream Bars.java

---
+++ medium/Maximum Ice Cream Bars.java
@@ -0,0 +1,14 @@
+//1833. Maximum Ice Creams Bars
+class Solution {
+  public int maxIceCream(int[] costs, int coins) {
+    Arrays.sort(costs);
+
+    for (int i = 0; i < costs.length; ++i)
+      if (coins >= costs[i])
+        coins -= costs[i];
+      else
+        return i;
+
+    return costs.length;
+  }
+}
